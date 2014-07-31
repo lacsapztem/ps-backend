1bac29dba3d1ac87376f727f3cc94194a19a39e7
lacsapztem <pascal.metz@gmail.com>
2014-07-31 12:39:37 +0200

    image unique

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,5 +2,6 @@ class Image < ActiveRecord::Base
 	validate :name, presence: true
 	validate :author, presence: true
 	validate :image, presence: true
+	validates_uniqueness_of :name, scope: :episode_id
 	belongs_to :episodes
 end
